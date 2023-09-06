642c1240dd03050e08a039d9fc530199461564f1
Kisaki Watanabe <wkisaki@gmail.com>
2023-09-06 13:16:30 -0700

    added serpAPI for google lens

--- models/research/image_search.py
+++ models/research/image_search.py
@@ -27,51 +27,44 @@ def main():
 
         params = {
             "api_key": "635b7526a4291ddec12eba9c3bdad8c7fdacf6ea91fad10e3b3c93add91aee7c", # https://serpapi.com/manage-api-key
-            "engine": "google_reverse_image",   # SerpApi search engine
-            "image_url": image_url,             # image URL to perform a reverse search
-            "location": "Dallas",               # location from where search comes from
+            "engine": "google_lens",   # SerpApi search engine
+            "url": image_url,             # image URL to perform a reverse search
             "hl": "en",                         # language of the search
-            "gl": "us"                          # country of the search
-            # other parameters
         }
         
         search = GoogleSearch(params)           # where data extraction happens on the SerpApi backend
         results = search.get_dict()             # JSON -> Python dictionary
 
-        # Check if "knowledge_graph" information is present
-        if "knowledge_graph" in results:
-            knowledge_graph = {
-                "title": results["knowledge_graph"].get("title"),
-                "description": results["knowledge_graph"].get("description")
-            }
+        if results.get("visual_matches"):
+            google_reverse_image_data[f"results for image {index}"]["visual_matches"] = []
 
-            google_reverse_image_data[f"results for image {index}"]["knowledge_graph"] = knowledge_graph
-
-
-        # Some queries may not include organic results
-        if results.get("image_results"):
-            google_reverse_image_data[f"results for image {index}"]["organic_results"] = []
-
-            for result in results["image_results"][:3]:  # Save only the top 3 results
-                image_results = {
+            for result in results["visual_matches"][:6]:  #  Save only the top 6 results
+                visual_matches = {
                     "position": result.get("position"),
                     "title": result.get("title"),
                     "link": result.get("link"),
-                    "snippet": result.get("snippet")
+                    "source": result.get("source"),
+                    "source_icon": result.get("source_icon"),
+                    "rating": result.get("rating"),
+                    "reviews": result.get("reviews"),
+                    "price": result.get("price"),
+                    "rating": result.get("rating"),
+                    "reviews": result.get("reviews")
                 }
 
-                google_reverse_image_data[f"results for image {index}"]["organic_results"].append(image_results)
+                google_reverse_image_data[f"results for image {index}"]["visual_matches"].append(visual_matches)
 
         # Some queries may not include this information
-        if results.get("inline_images"):
-            google_reverse_image_data[f"results for image {index}"]["inline_images"] = []
+        if results.get("text_results"):
+            google_reverse_image_data[f"results for image {index}"]["text_results"] = []
 
-            for result in results["inline_images"]:  # [:3] Save only the top 3 results:
-                google_reverse_image_data[f"results for image {index}"]["inline_images"].append({
-                    "source": result.get("source"),
-                    "thumbnail": result.get("thumbnail")
-                })
-
+            combined_text = []
+            for result in results["text_results"]:  # [:3] Save only the top 3 results:                
+                combined_text.append(result.get("text"))
+            
+            combined_text_dict = {"text": " ".join(combined_text)}
+            google_reverse_image_data[f"results for image {index}"]["text_results"].append(combined_text_dict)
+    
     return google_reverse_image_data
 
 if __name__ == "__main__":
