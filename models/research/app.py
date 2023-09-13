1cd1d599909fab6ab6d25efdeb4531bb2ab4bad2
Kisaki Watanabe <wkisaki@gmail.com>
2023-09-13 13:58:16 -0700

    change app file

--- models/research/app.py
+++ models/research/app.py
@@ -1,59 +1,67 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+#from flask import Flask, render_template, request, redirect, url_for
 import boto3
-from botocore.exceptions import NoCredentialsError
+#from botocore.exceptions import NoCredentialsError
+import datetime
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
 # Amazon S3 Configuration
 S3_BUCKET = "kiwi-cropped-image"
 S3_REGION = "us-west-1"
 
-# File to store URLs
-URLS_FILE = r"/Users/kisaki/Desktop/Kisaki_Personal_Folder/fast_api_sandbox/models/research/result_urls/uploaded_urls.txt"
+# Directory to retrieve images
+dirname = os.path.dirname(__file__)
+UPLOAD_DIRECTORY = os.path.join(dirname, 'outputs')
+# Directory to store URLs
+URLS_DIRECTORY = os.path.join(dirname,'result_urls')
 
 def save_url_to_file(url):
     with open(URLS_FILE, "a") as file:
         file.write(url + "\n")
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        # Check if a file was uploaded
-        if "file" not in request.files:
-            return redirect(request.url)
+def main():
+    uploaded_count = 0
 
-        file = request.files["file"]
+    # Upload the file to Amazon S3
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"), # set in .zshrc
+        aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"), # set in .zshrc
+        region_name=S3_REGION,
+    )
 
-        if file.filename == "":
-            return redirect(request.url)
+    for filename in os.listdir(UPLOAD_DIRECTORY):
+        if filename.endswith(".jpg"):  # You can specify the file extension you want to upload
+            file_path = os.path.join(UPLOAD_DIRECTORY, filename)
+            with open(file_path, "rb") as file:
+                s3.upload_fileobj(
+                    file,
+                    S3_BUCKET,
+                    os.path.join("images", filename),  # Object key in S3, adjust as needed
+                    ExtraArgs={"ACL": "public-read"},
+                )
 
-        try:
-            # Upload the file to Amazon S3
-            s3 = boto3.client(
-                "s3",
-                aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"), # set in .zshrc
-                aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"), # set in .zshrc
-                region_name=S3_REGION,
-            )
-            s3.upload_fileobj(
-                file,
-                S3_BUCKET,
-                file.filename,
-                ExtraArgs={"ACL": "public-read"},
-            )
+                url = f"https://{S3_BUCKET}.s3.{S3_REGION}.amazonaws.com/images/{filename}"
 
-            url = f"https://{S3_BUCKET}.s3.{S3_REGION}.amazonaws.com/{file.filename}"
+                # Save the URL to the text file
+                print(url)
+                save_url_to_file(url)
 
-            # Save the URL to the text file
-            save_url_to_file(url)
+                uploaded_count += 1
+    
+    # Check if all files have been uploaded - add assert logic 
 
-            return render_template("index.html", url=url)
+    return f"{uploaded_count} files  uploaded successfully."
 
-        except NoCredentialsError:
-            return "AWS credentials not available."
-
-    return render_template("index.html")
+        # except NoCredentialsError:
+        #     return "AWS credentials not available."
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Count the total number of files in the directory
+    total_files = len([filename for filename in os.listdir(UPLOAD_DIRECTORY) if filename.endswith(".jpg")])
+
+    date = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    URLS_FILE = os.path.join(URLS_DIRECTORY, "uploaded_urls_{}.txt".format(date))
+
+    main()
