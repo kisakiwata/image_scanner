0c514988aa6caaf88eb9900189e771f986694bba
Kisaki Watanabe <wkisaki@gmail.com>
2023-10-03 15:50:41 -0700

    updates for new uploaded images

--- models/research/app.py
+++ models/research/app.py
@@ -1,6 +1,8 @@
 import os
 import boto3
 import datetime
+import re
+import uuid
 
 # Amazon S3 Configuration
 S3_BUCKET = "kiwi-cropped-image"
@@ -8,9 +10,35 @@ S3_REGION = "us-west-1"
 
 # Directory to retrieve images
 dirname = os.path.dirname(__file__)
-UPLOAD_DIRECTORY = os.path.join(dirname, 'outputs')
+#UPLOAD_DIRECTORY = os.path.join(dirname, 'inference_output')
+image_folder = os.path.join(dirname, 'images')
+output_folder = os.path.join(dirname,'inference_output')
 # Directory to store URLs
 URLS_DIRECTORY = os.path.join(dirname,'result_urls')
+
+def get_recent_tile_images(image_folder, output_folder):
+    # Get the most recent image file from the 'image' folder
+    image_extensions = ('.jpg', '.jpeg', '.png')
+    image_files = [f for f in os.listdir(image_folder) if f.endswith(image_extensions)]
+    if not image_files:
+        return []  # No image files found in 'image' folder
+
+    most_recent_image = max(image_files, key=lambda x: os.path.getctime(os.path.join(image_folder, x)))
+
+    # Extract the tile pattern from the most recent image file name
+    tile_info = re.search(r'([^/]+)\.(jpg|jpeg|png)', most_recent_image)
+    if tile_info:
+        tile_pattern = tile_info.group(1)
+    else:
+        return []  # Couldn't extract tile information from the image file name
+
+    # Get the JPEG images from 'inference_output' folder with matching tile patterns
+    matching_images = []
+    for file_name in os.listdir(output_folder):
+        if file_name.endswith('.jpg') and tile_pattern in file_name:
+            matching_images.append(os.path.join(output_folder, file_name))
+
+    return matching_images
 
 def save_url_to_file(url):
     with open(URLS_FILE, "a") as file:
@@ -26,25 +54,36 @@ def main():
         aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"), # set in .zshrc
         region_name=S3_REGION,
     )
+    matching_images = get_recent_tile_images(image_folder, output_folder)
+    # for filename in os.listdir(UPLOAD_DIRECTORY):
+    #     if filename.endswith(".jpg"):  # You can specify the file extension you want to upload
+    #         file_path = os.path.join(UPLOAD_DIRECTORY, filename)
+    
+    for file_path in matching_images:
+        # Extract the original filename from the file path
+        original_filename = os.path.basename(file_path)
 
-    for filename in os.listdir(UPLOAD_DIRECTORY):
-        if filename.endswith(".jpg"):  # You can specify the file extension you want to upload
-            file_path = os.path.join(UPLOAD_DIRECTORY, filename)
-            with open(file_path, "rb") as file:
-                s3.upload_fileobj(
-                    file,
-                    S3_BUCKET,
-                    os.path.join("images", filename),  # Object key in S3, adjust as needed
-                    ExtraArgs={"ACL": "public-read"},
-                )
+        # Generate a unique identifier (UUID)
+        unique_id = str(uuid.uuid4())
 
-                url = f"https://{S3_BUCKET}.s3.{S3_REGION}.amazonaws.com/images/{filename}"
+        # Create a new unique filename by appending the UUID to the original filename
+        unique_filename = f"{unique_id}_{original_filename}"
 
-                # Save the URL to the text file
-                print(url)
-                save_url_to_file(url)
+        with open(file_path, "rb") as file:
+            s3.upload_fileobj(
+                file,
+                S3_BUCKET,
+                os.path.join("images", unique_filename),  # Object key in S3, adjust as needed
+                ExtraArgs={"ACL": "public-read"},
+            )
 
-                uploaded_count += 1
+            url = f"https://{S3_BUCKET}.s3.{S3_REGION}.amazonaws.com/images/{unique_filename}"
+
+            # Save the URL to the text file
+            print(url)
+            save_url_to_file(url)
+
+            uploaded_count += 1
     
     # Check if all files have been uploaded - add assert logic 
 
@@ -54,8 +93,6 @@ def main():
         #     return "AWS credentials not available."
 
 if __name__ == "__main__":
-    # Count the total number of files in the directory
-    total_files = len([filename for filename in os.listdir(UPLOAD_DIRECTORY) if filename.endswith(".jpg")])
 
     date = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
     URLS_FILE = os.path.join(URLS_DIRECTORY, "uploaded_urls_{}.txt".format(date))
--- models/research/scan.py
+++ models/research/scan.py
@@ -60,31 +60,6 @@ with st.container():
 # Markdown
 st.markdown("Input Image")
 
-# File uploader
-uploaded_file = st.file_uploader("Upload a file", type=["jpg", "png"])
-if uploaded_file is not None:
-    st.write("Image uploaded!")
-    st.image(uploaded_file, caption=None, width=1200, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
-
-# Display JSON
-st.subheader("Product Information")
-dirname = os.path.dirname(__file__)
-json_directory = os.path.join(dirname,'webscrape_result')
-json_data = retrieve_json(json_directory)
-
-# Dataframe
-df = pd.DataFrame(json_data)
-cols = df.columns
-new_cols = ['product_id','product_name',  'in_stock', 'product_brand_1', 'product_brand_2',
-       'product_category_1', 'product_category_2', 'product_category_3',
-       'product_review_rating_count', 'product_review_stars', 'product_price', 
-       'product_dimensions', 'product_item_weight', 'product_unit',
-       'store_location_zipcode', 'store_location_state', 'store_location_city',
-       'store_id', 'product_link', 'brand_link', 'inventory_stockLevel',
-       'tcin']
-df = df[new_cols]
-df = df.rename(columns={'product_brand_1': 'retail', 'product_brand_2': 'brand'})
-
 with st.sidebar:
     # Selectbox 
     st.markdown("Input Location")
@@ -92,49 +67,63 @@ with st.sidebar:
     zcdb = ZipCodeDatabase()
     city_state = zcdb[location].city + ", " + zcdb[location].state
     st.write('Current search location is', city_state)
-    selected_options = st.multiselect("Filter by retail", list(set(df["retail"]))) #, default=specific_source
-    
 
-#st.write("You selected:", [value for value in selected_options])
+# File uploader
+uploaded_file = st.file_uploader("Upload a file", type=["jpg", "png", "jpeg"])
+dirname = os.path.dirname(__file__)
+iamge_directory = os.path.join(dirname,'images')
+if uploaded_file is not None:
+        with open(os.path.join(iamge_directory, uploaded_file.name), "wb") as f:
+            f.write(uploaded_file.read())
+        st.write("Image uploaded!")
+        st.image(uploaded_file, caption=None, width=1200, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 
-# Filter the original DataFrame to only include rows with the selected colors
-df_filtered = df[df['retail'].isin(selected_options)]
+# run the inference and web scraping with this
+subprocess.run("python models/research/process.py", shell=True)
 
-#convert dataframe to adgrid
-gb = GridOptionsBuilder.from_dataframe(df_filtered)
-gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
-gb.configure_side_bar() #Add a sidebar
-gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
-gridOptions = gb.build()
+# Display JSON
+st.subheader("Product Information")
+json_directory = os.path.join(dirname,'webscrape_result')
+json_data = retrieve_json(json_directory)
 
-# grid_response = AgGrid(
-#     df,
-#     gridOptions=gridOptions,
-#     data_return_mode='AS_INPUT', 
-#     update_mode='MODEL_CHANGED', 
-#     fit_columns_on_grid_load=False,
-#     #theme='light', #Add theme color to the table
-#     enable_enterprise_modules=True,
-#     height=350, 
-#     width='100%',
-#     reload_data=True
-# )
 
-# #data = grid_response['data']
-# selected = grid_response[selected_options] 
-# df_grid = pd.DataFrame(data)
-
-# Display the DataFrame using ag-Grid
-#st.subheader("Filtered Data")
+# Dataframe
+df = pd.DataFrame(json_data)
+cols = df.columns
 if not df.empty:
+    new_cols = ['product_id','product_name',  'in_stock', 'product_brand_1', 'product_brand_2',
+        'product_category_1', 'product_category_2', 'product_category_3',
+        'product_review_rating_count', 'product_review_stars', 'product_price', 
+        'product_dimensions', 'product_item_weight', 'product_unit',
+        'store_location_zipcode', 'store_location_state', 'store_location_city',
+        'store_id', 'product_link', 'brand_link', 'inventory_stockLevel',
+        'tcin']
+    df = df[new_cols]
+    df = df.rename(columns={'product_brand_1': 'retail', 'product_brand_2': 'brand'})
+    selected_options = st.multiselect("Filter by retail", list(set(df["retail"]))) #, default=specific_source
+
+    # Filter the original DataFrame to only include rows with the selected colors
+    df_filtered = df[df['retail'].isin(selected_options)]
+
+    #convert dataframe to adgrid
+    gb = GridOptionsBuilder.from_dataframe(df_filtered)
+    gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+    gb.configure_side_bar() #Add a sidebar
+    gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
+    gridOptions = gb.build()
+
     gb = GridOptionsBuilder.from_dataframe(df_filtered)
     grid_response = AgGrid(df_filtered, gridOptions=gb.build(), width="100%", height=400,
     data_return_mode='AS_INPUT', 
     update_mode='MODEL_CHANGED', 
     fit_columns_on_grid_load=False,)
 else:
-    st.write("No data found for the selected options:", selected_options)
+    st.write("No data found")
 
+#st.subheader("Filtered Data")
+
+#else:
+    #st.write("No data found for the selected options:", selected_options)
 
 # # ---- CONTACT ----
 # with st.container():
